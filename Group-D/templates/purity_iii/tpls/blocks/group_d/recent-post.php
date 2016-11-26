790d66c535ce28f935240fdb119fdc74346a0d55
Hoan <nguyenanhhoanld@gmail.com>
2016-11-26 22:43:07 +0700

    Author: Hoan. Date: Nov 26, 2016 - 10:42PM. Description: Clear project Joomla.

--- Group-D/templates/purity_iii/tpls/blocks/group_d/recent-post.php
+++
@@ -1,9 +0,0 @@
-<body>
-	<div class="style">
-    	<div class="container">
-			<div class="recent-post container">
-			    <?php include 'recent-post-title.php' ?>
-			</div>
-		</div>
-	</div>
-</body>
